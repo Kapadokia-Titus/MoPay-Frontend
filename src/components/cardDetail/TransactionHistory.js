bbfc7628a1dacce93fb4af4cdf40621f3c320246
kapadokia <ktnyandoch@gmail.com>
2022-12-10 01:53:57 +0300

    Updated Transaction History

--- src/components/cardDetail/TransactionHistory.js
+++ src/components/cardDetail/TransactionHistory.js
@@ -1,162 +1,57 @@
-import { List, FlexboxGrid } from 'rsuite';
-import ImageIcon from '@rsuite/icons/legacy/Image';
-import FilmIcon from '@rsuite/icons/legacy/Film';
-import UserCircleIcon from '@rsuite/icons/legacy/UserCircleO';
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { Table, Button } from 'rsuite';
 
-const data = [
-  {
-    title: 'Hong Kong Free Walk @ Tsim Sha Tsui',
-    icon: <ImageIcon />,
-    creator: 'Yvnonne',
-    date: '2017.10.13 14:50',
-    peak: 3223,
-    peakRaise: 433,
-    uv: 433,
-    uvRaise: 33
-  },
-  {
-    title: 'Celebrate Mid-Autumn Festival',
-    icon: <ImageIcon />,
-    creator: 'Daibiao',
-    date: '2017.10.13 14:50',
-    peak: 3223,
-    peakRaise: 238,
-    uv: 238,
-    uvRaise: 28
-  },
-  {
-    title: 'Live basketball',
-    icon: <FilmIcon />,
-    creator: 'Bidetoo',
-    date: '2017.10.13 14:50',
-    peak: 4238,
-    peakRaise: -239,
-    uv: 239,
-    uvRaise: 29
-  },
-  {
-    title: 'Legislative Yuan meeting live',
-    icon: <FilmIcon />,
-    creator: 'Yvnonne',
-    date: '2017.10.13 14:50',
-    peak: 4238,
-    peakRaise: 2321,
-    uv: 921,
-    uvRaise: 91
-  },
-  {
-    title: 'Iwank Patch',
-    icon: <ImageIcon />,
-    creator: 'Tony',
-    date: '2017.10.13 14:50',
-    peak: 2321,
-    peakRaise: 1321,
-    uv: 321,
-    uvRaise: 132
-  }
-];
+const { Column, HeaderCell, Cell } = Table;
 
-const styleCenter = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '60px'
-};
+export const Transaction = ({data}) => {
+    const [transactions, setTransactions] = useState([])
 
-const slimText = {
-  fontSize: '0.666em',
-  color: '#97969B',
-  fontWeight: 'lighter',
-  paddingBottom: 5
-};
-
-const titleStyle = {
-  paddingBottom: 5,
-  whiteSpace: 'nowrap',
-  fontWeight: 500
-};
-
-const dataStyle = {
-  fontSize: '1.2em',
-  fontWeight: 500
-};
-
-export const Transaction = () => {
-  const renderRaise = React.useCallback(number => {
-    const isPositive = number > 0;
-    const isNegative = number < 0;
+    useEffect(()=>{
+        data?.map(card=>{
+            fetch(`https://mopay-production.up.railway.app/card/${card.id}`)
+            .then(res =>res.json)
+            .then(data => setTransactions(data.payments))
+        })
+    })
     return (
-      <span style={{ paddingLeft: 15, color: isNegative ? 'red' : 'green' }}>
-        <span>{isPositive ? '+' : null}</span>
-        <span>{number}</span>
-      </span>
-    );
-  }, []);
-
-  return (
-    <List hover>
-      {data.map((item, index) => (
-        <List.Item key={item['title']} index={index + 1}>
-          <FlexboxGrid>
-            {/*icon*/}
-            <FlexboxGrid.Item colspan={2} style={styleCenter}>
-              {React.cloneElement(item['icon'], {
-                style: {
-                  color: 'darkgrey',
-                  fontSize: '1.5em'
-                }
-              })}
-            </FlexboxGrid.Item>
-            {/*base info*/}
-            <FlexboxGrid.Item
-              colspan={6}
-              style={{
-                ...styleCenter,
-                flexDirection: 'column',
-                alignItems: 'flex-start',
-                overflow: 'hidden'
-              }}
-            >
-              <div style={titleStyle}>{item['title']}</div>
-              <div style={slimText}>
-                <div>
-                  <UserCircleIcon />
-                  {' ' + item['creator']}
-                </div>
-                <div>{item['date']}</div>
-              </div>
-            </FlexboxGrid.Item>
-            {/*peak data*/}
-            <FlexboxGrid.Item colspan={6} style={styleCenter}>
-              <div style={{ textAlign: 'right' }}>
-                <div style={slimText}>Peak</div>
-                <div style={dataStyle}>{item['peak'].toLocaleString()}</div>
-              </div>
-              {renderRaise(item['peakRaise'])}
-            </FlexboxGrid.Item>
-            {/*uv data*/}
-            <FlexboxGrid.Item colspan={6} style={styleCenter}>
-              <div style={{ textAlign: 'right' }}>
-                <div style={slimText}>User visits (UV)</div>
-                <div style={dataStyle}>{item['uv'].toLocaleString()}</div>
-              </div>
-              {renderRaise(item['uvRaise'])}
-            </FlexboxGrid.Item>
-            {/*uv data*/}
-            <FlexboxGrid.Item
-              colspan={4}
-              style={{
-                ...styleCenter
-              }}
-            >
-              <a href="#">View</a>
-              <span style={{ padding: 5 }}>|</span>
-              <a href="#">Edit</a>
-            </FlexboxGrid.Item>
-          </FlexboxGrid>
-        </List.Item>
-      ))}
-    </List>
+        <Table
+          height={400}
+          data={transactions}
+          onRowClick={rowData => {
+            console.log(rowData);
+          }}
+        >
+          <Column width={60} align="center" fixed>
+            <HeaderCell>Id</HeaderCell>
+            <Cell dataKey="id" />
+          </Column>
+    
+          <Column width={150}>
+            <HeaderCell>Card Number</HeaderCell>
+            <Cell dataKey="card_number" />
+          </Column>
+    
+          <Column width={150}>
+            <HeaderCell>Card Name</HeaderCell>
+            <Cell dataKey="card_name" />
+          </Column>
+    
+          <Column width={100}>
+            <HeaderCell>Ammount</HeaderCell>
+            <Cell dataKey="ammount" />
+          </Column>
+    
+          <Column width={100}>
+            <HeaderCell>Status</HeaderCell>
+            <Cell dataKey="status" />
+          </Column>
+    
+    
+          <Column width={300}>
+            <HeaderCell>Email</HeaderCell>
+            <Cell dataKey="receiver_email" />
+          </Column>
+          
+        </Table>
   );
 };
